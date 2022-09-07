ebe921c4a134c38daa9e7e278db4fa47ee573fab
samrath-sudesh-acharya <samrathsacharya@gmail.com>
2022-09-08 01:08:24 +0530

    Lastest code

--- model.py
+++ model.py
@@ -6,49 +6,49 @@ class User(BaseModel):
     password:str
 
 class Arrival(BaseModel):
-    arrival_airport: str
-    arrival_timezone: str
-    arrival_iata: str
-    arrival_icao: str
-    arrival_terminal: str
-    arrival_gate: str
+    arrival_airport: Optional[str]
+    arrival_timezone: Optional[str]
+    arrival_iata: Optional[str]
+    arrival_icao: Optional[str]
+    arrival_terminal: Optional[str]
+    arrival_gate: Optional[str]
     arrival_baggage: Optional[int]
     arrival_delay: Optional[int]
-    arrival_scheduled: str
-    arrival_estimated: str
-    arrival_actual: str
-    arrival_estimated_runway: str
-    arrival_actual_runway: str
+    arrival_scheduled: Optional[str]
+    arrival_estimated: Optional[str]
+    arrival_actual: Optional[str]
+    arrival_estimated_runway: Optional[str]
+    arrival_actual_runway: Optional[str]
 
 class Departure(BaseModel):
-    departure_airport: str
-    departure_timezone: str
-    departure_iata: str
-    departure_icao: str
-    departure_terminal: str
-    departure_gate: str
+    departure_airport: Optional[str]
+    departure_timezone: Optional[str]
+    departure_iata: Optional[str]
+    departure_icao: Optional[str]
+    departure_terminal: Optional[str]
+    departure_gate: Optional[str]
     departure_baggage: Optional[int]
     departure_delay: Optional[int]
-    departure_scheduled: str
-    departure_estimated: str
-    departure_actual: str
-    departure_estimated_runway: str
-    departure_actual_runway: str
+    departure_scheduled: Optional[str]
+    departure_estimated: Optional[str]
+    departure_actual: Optional[str]
+    departure_estimated_runway: Optional[str]
+    departure_actual_runway: Optional[str]
 
 class Airline(BaseModel):
-    airline_name: str
-    airline_iata: str
-    airline_icao: str
+    airline_name: Optional[str]
+    airline_iata: Optional[str]
+    airline_icao: Optional[str]
 
 class Flight(BaseModel):
-    flight_number: str
-    flight_iata: str
-    flight_icao: str
+    flight_number: Optional[str]
+    flight_iata: Optional[str]
+    flight_icao: Optional[str]
 
 class Aircraft(BaseModel):
-    user_id: int
-    flight_date: str
-    flight_status: str
+    user_id: Optional[int]
+    flight_date: Optional[str]
+    flight_status: Optional[str]
 
 class Data(Aircraft,Flight,Arrival,Departure,Airline):
     class Config:
